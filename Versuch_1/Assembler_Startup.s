a913ef3230c4013080f4f20e0a23ce58ebbfdce8
Hanan Ahmed Ashir <78960911+z0mbiE098@users.noreply.github.com>
2025-12-14 16:45:47 +0100

    Add files via upload

--- Versuch_1/Assembler_Startup.s
+++ Versuch_1/Assembler_Startup.s
@@ -26,26 +26,24 @@
 ;********************************************************************
 				AREA		Daten, DATA, READWRITE
 Datenanfang
-
-
 ;********************************************************************
 ;* Programm-Bereich bzw. Programm-Speicher							*
 ;********************************************************************
 				AREA		Programm, CODE, READONLY
 				ARM
 Reset_Handler	MSR			CPSR_c, #0x10	; User Mode aktivieren
-;STRING          DCB     "65535",0      ; '\0'-terminierter String
-X 				DCD 30
-Y				DCD 0
+
+
 ;********************************************************************
 ;* Hier das eigene (Haupt-)Programm einfuegen   					*
 ;********************************************************************
                 ;LDR     R0, =STRING     ; R0 = &STRING (Adresse von String wird in R0 geladen)
                 ;BL      AtoI            ; AtoI wird gerufen
 			
-				LDR 	R0, =X
-				LDR 	R0, [R0]
-				BL Formel
+				LDR 	R0, =NUMBER	;Replace String mit X for Aufgabe_2
+				;LDR 	R0, [R0]		;Zeile für Aufgabe 2
+				LDR 	R0, [R0]		;Zeile für Aufgabe 3
+				BL uItoBCD					; Funktion Aufrufen
 ;********************************************************************
 ;* Ende des eigenen (Haupt-)Programms                               *
 ;********************************************************************
@@ -56,6 +54,7 @@ endlos			B			endlos
 ;********************************************************************
 CHAR_0      EQU     0x30    ; '0'
 
+;------------------------Aufgabe 1-----------------------------------
 AtoI
                 MOV     R2, #0          ; Hier wird Result gespeichert
                 MOV     R3, #0          ; Vorzeichen Flag. 0 = positiv, 1 = negative
@@ -107,6 +106,7 @@ AtoI_Positive
                 BX      LR              ; zur?ck zum Aufrufer
 
 
+;------------------------Aufgabe 2-----------------------------------
 Formel
 				MOV R1, R0
 				
@@ -124,13 +124,46 @@ DivideLoop
 DivDone
 				MOV R0, R3, LSL #2
 				BX LR
+;------------------------Aufgabe 3-----------------------------------
 
+uItoBCD
+				MOV R1, #0				; Ergebnis wird hier gespeichert 
+				MOV R2, #0				; R2 = Shiftzähler
+
+uItoBCD_Loop
+				CMP R0, #0
+				BEQ uItoBCD_Done
+				
+				MOV R3, R0
+				MOV R4, #0
+				
+Division_10
+				CMP R3, #10
+				BLT Division_END
+				SUB R3, R3, #10
+				ADD R4, R4, #1
+				B Division_10
+				
+Division_END				
+				MOV R5, R3, LSL R2
+				ORR R1, R1, R5
+				ADD R2, R2, #4
+				MOV R0, R4
+				B uItoBCD_Loop
+				
+
+uItoBCD_Done
+				MOV R0, R1
+				BX LR
+
+;------------------------Aufgabe 4-----------------------------------
                 
 ;********************************************************************
 ;* Konstanten im CODE-Bereich                                       *
 ;********************************************************************
-
-
+X 				DCD 30
+STRING          DCB     "-65535",0      ; '\0'-terminierter String
+NUMBER 			DCD		65535
 ;********************************************************************
 ;* Ende der Programm-Quelle                                         *
 ;********************************************************************
